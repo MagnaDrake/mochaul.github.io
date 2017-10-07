690275c864d4bd731b93e75df3694e7560f68d9b
Mochaul <praditomo@gmail.com>
2017-10-07 19:55:06 +0700

    delete page

--- FunCoding2017/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Spritesheet" tilewidth="32" tileheight="32" spacing="5" margin="6" tilecount="165" columns="15">
- <image source="C:/Users/Firas/Downloads/Spritesheet.png" width="561" height="414"/>
-</tileset>
